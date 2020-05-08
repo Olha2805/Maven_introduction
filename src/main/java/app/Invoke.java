b61cf42bf618ca0602181b8de7c540c5e2225403
Olha <qwerty@mail.ru>
2020-05-08 16:48:55 +0300

    with pom

--- src/main/java/app/Invoke.java
+++ src/main/java/app/Invoke.java
@@ -1,4 +1,9 @@
 package app;
 
 public class Invoke {
+    public static void main(String[] args) {
+        Server server = new Server();
+        System.out.println(server.getUsers());
+    }
 }
+
--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -1,4 +1,14 @@
 package app;
 
 public class Server {
+
+    private String [] arr = {"Vasia", "Vova", "Sasha"};
+
+    public String getUsers (){
+        String result = "";
+        for (int i = 0; i < this.arr.length; i++){
+            result += this.arr[i] + (i < this.arr.length - 1 ? ", " : "");
+        }
+        return result;
+    }
 }
